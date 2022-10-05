8432242b9c2de28d5a31cfc660026ecf20843e02
masumbillah360 <masumbillah6584@gmail.com>
2022-10-05 13:16:14 +0600

    show all food item number

--- src/compo/AllFoods.js
+++ src/compo/AllFoods.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import SingleFood from './SingleFood';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import SingleFood from "./SingleFood";
 
 const AllFoods = () => {
-    const allFood = useLoaderData()
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
-            {
-                allFood.meals.map((meal)=> <SingleFood meal ={meal} key={meal.idMeal}></SingleFood>)
-            }
-        </div>
-    );
+	const allFood = useLoaderData();
+	return (
+		<div>
+			<h1>Total Items : {allFood.meals.length}</h1>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+				{allFood.meals.map((meal) => (
+					<SingleFood meal={meal} key={meal.idMeal}></SingleFood>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default AllFoods;
--- src/compo/Home.js
+++ src/compo/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     const cat = useLoaderData()
     return (
         <div>
-            <h1>Food Categories</h1>
+            <h1>Food Categories({cat.categories.length})</h1>
             <article className='grid grid-cols-3 gap-3'>
                 {
                     cat.categories.map((cats)=> <Categories meal={cats} key={cats.idCategory}></Categories>)
