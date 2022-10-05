1f88d04307242dc162f16c904b8034d56d14dd08
masumbillah360 <masumbillah6584@gmail.com>
2022-10-05 13:26:38 +0600

    added search bar on navbar

--- src/compo/Navbar.js
+++ src/compo/Navbar.js
@@ -3,12 +3,16 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <nav className='flex justify-center items-center h-8 bg-gray-800 text-white'>
-                <NavLink className='mr-3 px-3 bg-gray-600 rounded-lg' to='/home' >Home</NavLink>
-                <NavLink className='mr-3 px-3 bg-gray-600 rounded-lg' to='/blog' >Blog</NavLink>
-                <NavLink className='mr-3 px-3 bg-gray-600 rounded-lg' to='/friends' >Friends</NavLink>
+        <div className='flex justify-center flex-col items-center h-40 bg-gray-600'>
+            <nav className='text-white'>
+                <NavLink className='mr-3 px-3 bg-gray-800 rounded-lg' to='/home' >Home</NavLink>
+                <NavLink className='mr-3 px-3 bg-gray-800 rounded-lg' to='/blog' >Blog</NavLink>
+                <NavLink className='mr-3 px-3 bg-gray-800 rounded-lg' to='/friends' >Friends</NavLink>
             </nav>
+            <div className='mt-4'>
+                <input className='rounded-l px-1' type="text" placeholder='Search Your Food...'/>
+                <button className='bg-amber-600 px-2 rounded-r text-white font-bold'>Search</button>
+            </div>
         </div>
     );
 };
